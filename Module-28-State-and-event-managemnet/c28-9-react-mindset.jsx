708b126b80121fcd3fe0fbceebbb030cc566634c
Raju204116 <raju204116@gmail.com>
2026-09-03 18:20:34 +0600

    c28-9-react-mindset

---
+++ Module-28-State-and-event-managemnet/c28-9-react-mindset.jsx
@@ -0,0 +1,34 @@
+/* 
+✅✅Know about the basic concepts of React going to react.dev => references
+
+✅Why hook is called hook?
+✅what is single page application?
+
+✅Think What makes a Good Component
+        -Has a clear responsibility
+        -Can be understood independently
+        -Can be reused when appropriate
+        -Receives data through props when needed
+        -Keeps related logic together
+-Don't Over-Componentize
+
+
+✅✅Building web using The traditional way vs the React way 
+
+
+✅Steps to Complete a React web :
+    -Start with the UI (Design)
+    -Break the UI into Components
+    -Identify the data
+    -Identify the States
+    -Decide state ownership :  Where should State Live when multiple task need the same component
+    -Pass data using Props
+    -Build Components
+    -Connect Interactions
+    
+
+
+
+
+
+*/
